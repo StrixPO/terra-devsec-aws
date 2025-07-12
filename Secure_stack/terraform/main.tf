7e2dab484e5ca59f725666efe70fd7c49c7a5e6e
StrixPO <rusarrp@gmail.com>
2025-07-12 02:49:29 +0000

    CI integration fixes

--- Secure_stack/terraform/main.tf
+++ Secure_stack/terraform/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket         = "psstbin-tf-remote-state"
+    key            = "psstbin/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "psstbin-tf-locks"
+    encrypt        = true
+  }
+}
+
+
 module "vpc" {
   source          = "./modules/vpc"
   project         = var.project
