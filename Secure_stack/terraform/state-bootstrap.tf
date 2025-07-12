26196caa4f83bffa97a8d832b6860e222723f56d
StrixPO <rusarrp@gmail.com>
2025-07-12 02:38:08 +0000

    CI integration fixes

--- Secure_stack/terraform/state-bootstrap.tf
+++ Secure_stack/terraform/state-bootstrap.tf
@@ -1,6 +1,6 @@
 
 resource "aws_s3_bucket" "tf_state" {
-  bucket = "psstbin-tf-remote-state"
+  bucket        = "psstbin-tf-remote-state"
   force_destroy = true
 
   versioning {
@@ -13,9 +13,9 @@ resource "aws_s3_bucket" "tf_state" {
 }
 
 resource "aws_dynamodb_table" "tf_lock" {
-  name           = "psstbin-tf-locks"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
+  name         = "psstbin-tf-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
   attribute {
     name = "LockID"
